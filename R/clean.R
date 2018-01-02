0c7fe376074e9128c04fdc3b2922c0179aa41941
Miguel Alvarez <kamapu78@gmail.com>
2018-01-02 11:01:48 +0100

    Function 'clean' set classes of keys

--- R/clean.R
+++ R/clean.R
@@ -29,6 +29,18 @@ setMethod("clean", signature(object="taxlist"),
             # clean parents (deleted parents)
             object@taxonRelations$Parent[!object@taxonRelations$Parent %in%
                             object@taxonRelations$TaxonConceptID] <- NA
-            return(object)
+			# clean classes of key fields
+			object@taxonNames$TaxonConceptID <- as.integer(object@taxonNames$TaxonConceptID)
+			object@taxonNames$TaxonUsageID <- as.integer(object@taxonNames$TaxonUsageID)
+			object@taxonNames$TaxonName <- paste(object@taxonNames$TaxonName)
+			object@taxonNames$AuthorName <- paste(object@taxonNames$AuthorName)
+			object@taxonRelations$TaxonConceptID <- as.integer(object@taxonRelations$TaxonConceptID)
+			object@taxonRelations$AcceptedName <- as.integer(object@taxonRelations$AcceptedName)
+			object@taxonRelations$Basionym <- as.integer(object@taxonRelations$Basionym)
+			object@taxonRelations$Parent <- as.integer(object@taxonRelations$Parent)
+			object@taxonRelations$ViewID <- as.integer(object@taxonRelations$ViewID)
+			object@taxonTraits$TaxonConceptID <- as.integer(object@taxonTraits$TaxonConceptID)
+			object@taxonViews$ViewID <- as.integer(object@taxonViews$ViewID)
+			return(object)
         }
 )
