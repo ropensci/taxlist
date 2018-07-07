97122cc86047a4127a890c623e9e8ed7916581a1
Miguel Alvarez <kamapu78@gmail.com>
2018-07-07 10:46:19 +0200

    New function 'clean_strings'

---
+++ R/clean_strings.R
@@ -0,0 +1,38 @@
+# TODO:   Method cleaning character strings
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+# Generic method
+setGeneric("clean_strings",
+		function(x, ...)
+			standardGeneric("clean_strings"))
+
+# Method for character
+setMethod("clean_strings", signature(x="character"),
+		function(x, from="utf8", to="utf8") {
+			x <- iconv(x, from, to)
+			x <- trimws(x, "both")
+			x <- gsub("\\s+", " ", x)
+			return(x)
+		}
+)
+
+# Method for factor
+setMethod("clean_strings", signature(x="factor"),
+		function(x, from="utf8", to="utf8") {
+			levels(x) <- clean_strings(levels(x), from, to)
+			return(x)
+		}
+)
+
+# Method for data.frame
+setMethod("clean_strings", signature(x="data.frame"),
+		function(x, from="utf8", to="utf8") {
+			for(i in colnames(x)) {
+				if(is.character(x[,i]) | is.factor(x[,i]))
+					x[,i] <- clean_strings(x[,i], from, to)
+			}
+			return(x)
+		}
+)
--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
 
 * Improve documentation in `print_name`
 * Solve problems with `as.phylo` (communication with developer of package `ape`)
-* Check for missing
+* Check for function `tnrs` and skip it from vignette
