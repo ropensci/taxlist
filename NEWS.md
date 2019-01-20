5da5364ff1ef08314d4a30a69008cb49d300b3cd
Miguel Alvarez <kamapu78@gmail.com>
2019-01-20 08:41:24 +0100

    new function 'count_taxa'

--- NEWS.md
+++ NEWS.md
@@ -3,7 +3,7 @@ taxlist 0.1.6
 
 ### New Features
 
-* New methods `as.phylo` and `as.multiPhylo` for plotting parent-child relations in dendrograms
+* New function `count_taxa()`
 
 ### Improvements
 * A new option `style="knitr"` for function `print_name()` (See [this issue](https://stackoverflow.com/questions/51092103/formatted-scientific-names-from-r-to-latex-using-sweave-or-knitr)) at **Stack Overflow**.
---
+++ R/count_taxa.R
@@ -0,0 +1,37 @@
+# TODO:   Counting taxa by their names
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+# Generic function
+setGeneric("count_taxa",
+		function(object, ...)
+			standardGeneric("count_taxa")
+)
+
+# Method for character vectors
+setMethod("count_taxa", signature(object="character"),
+		function(object, rm.na=TRUE, ...) {
+			if(rm.na) object <- object[!is.na(object)]
+			return(length(unique(object)))
+		}
+)
+
+# Method for factors
+setMethod("count_taxa", signature(object="factor"),
+		function(object, rm.na=TRUE, ...) {
+			if(rm.na) object <- object[!is.na(object)]
+			return(count_taxa(paste(object)))
+		}
+)
+
+# Method for taxlist objects
+setMethod("count_taxa", signature(object="taxlist"),
+		function(object, level, ...) {
+			if(missing(level))
+				n_taxa <- nrow(object@taxonRelations) else
+				n_taxa <- nrow(object@taxonRelations[
+								paste(object@taxonRelations$Level) == level,])
+			return(n_taxa)
+		}
+)
