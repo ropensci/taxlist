5944c32b4a44d6fe4f310e2d921fb27e9e3107e3
ma <ma@ma-ThinkStation-S20>
2019-10-12 14:53:56 +0200

    bug in tests

--- tests/testthat/test-tax2traits.R
+++ tests/testthat/test-tax2traits.R
@@ -6,7 +6,7 @@
 context("inserting taxonomic information in slot taxonTraits")
 
 test_that("comparing columns in taxon traits", {
-			expect_equal(rncol(Easplist@taxonTraits) <
+			expect_equal(ncol(Easplist@taxonTraits) <
 							ncol(tax2traits(Easplist)@taxonTraits), TRUE)
 		}
 )
