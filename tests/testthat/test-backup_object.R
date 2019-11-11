ad3bfccfaa901527a762f660841fff0d39ab7106
Miguel Alvarez <kamapu78@gmail.com>
2019-11-11 22:58:08 +0100

    adding backup_object to test suite

---
+++ tests/testthat/test-backup_object.R
@@ -0,0 +1,15 @@
+# TODO:   Add comment
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+context("Creating backups")
+
+# Loading installed examples
+load(file.path(path.package("taxlist"), "taxlist_examples/examples.Rda"))
+
+test_that("Backups of objects are written", {
+			expect_output_file(backup_object(data1, file="backup", stamp=FALSE,
+							overwrite=TRUE), "backup.rda")
+		}
+)
--- tests/testthat/test-taxon_relations.R
+++ tests/testthat/test-taxon_relations.R
@@ -5,6 +5,9 @@
 
 context("manipulating taxon concepts")
 
+# Loading installed examples
+load(file.path(path.package("taxlist"), "taxlist_examples/examples.Rda"))
+
 test_that("function taxon_relations is working", {
 			expect_is(taxon_relations(Easplist), "data.frame")
 			tmp <- Easplist
@@ -33,6 +36,17 @@ test_that("function add_concept is working", {
 			Diospyros <- subset(Easplist, grepl("Diospyros", TaxonName),
 					slot="names", keep_parents=TRUE)
 			expect_error(add_concept(Diospyros, Euclea))
+			# Working with lists without information on taxonomic levels
+			expect_equal(nrow(add_concept(data2,
+									TaxonName="Planta incognita")@taxonRelations) >
+					nrow(data2@taxonRelations), TRUE)
+			expect_warning(add_concept(data2, TaxonName="Planta incognita",
+							Level="species"))
+			# Adding on a new object
+			expect_equal(nrow(add_concept(new("taxlist"),
+									TaxonName="Planta incognita")@taxonRelations),
+					1)
+			
 		}
 )
 
