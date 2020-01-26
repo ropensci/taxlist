a6ae7186773efab3407ce5ae62af9c65585be466
ma <ma@ma-ThinkStation-S20>
2020-01-26 19:03:20 +0100

    A statistic section added to README

--- R/StartMessage.R
+++ R/StartMessage.R
@@ -2,6 +2,4 @@
     packageStartupMessage("This is taxlist ",
             utils::packageDescription("taxlist", field="Version"),
             appendLF=TRUE)
-	from_taxize <- c("apg_families","apg_orders","theplantlist")
-	rm(list=ls()[ls() %in% from_taxize])
 }
