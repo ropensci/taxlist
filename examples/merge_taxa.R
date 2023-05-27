6b42d83cb3e05068a287703e2cf841f8c4b8dd80
Miguel Alvarez <kamapu78@gmail.com>
2023-05-27 08:53:19 +0200

    New features in function 'merge_taxa()'

--- examples/merge_taxa.R
+++ examples/merge_taxa.R
@@ -3,3 +3,15 @@ summary(Easplist, c(206, 197))
 
 merged_cyperus <- merge_taxa(object = Easplist, concepts = c(206, 197),
     print_output = TRUE)
+
+## Subset with Kyllinga species
+ky <- subset(Easplist, TaxonName == "Kyllinga", keep_children = TRUE,
+    keep_parents = TRUE)
+ky
+indented_list(ky)
+
+## Merge to species and family
+merge_taxa(ky, level = c("species", "family"))
+
+## Merge to variety and genus
+merge_taxa(ky, level = c("variety", "genus"))
